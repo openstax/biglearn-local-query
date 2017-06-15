6ba44b582c974992e14fe2e0890763055baad58c
Dante Soares <dante.m.soares@rice.edu>
2017-06-15 11:25:50 -0500

    Use Time.current instead of Time.now

--- lib/worker.rb
+++ lib/worker.rb
@@ -12,14 +12,14 @@ class Worker
   end
 
   def run(run_every = 1.second)
-    start_time = Time.now.freeze
+    start_time = Time.current.freeze
     log(:info) { "Started at #{start_time}" }
 
     1.upto(Float::INFINITY).each do |iteration|
       run_once
 
       wake_up_at = start_time + iteration * run_every
-      sleep_interval = wake_up_at - Time.now
+      sleep_interval = wake_up_at - Time.current
       if sleep_interval > 0
         log(:debug) { "#{sleep_interval} second(s) ahead of schedule - sleeping..." }
         sleep sleep_interval
